f3acaccee101246c81d2882b30ba142f8bb5ebed
kamel-ah <kamelahmed147@gmail.com>
2021-02-08 19:30:44 +0100

    Add files via upload

---
+++ src/meteo.js
@@ -0,0 +1,24 @@
+let villeCourante = "Lyon";
+function showCity() {
+  let cities = document.getElementById("pet-select").value;
+  console.log(cities);
+  let l = cities.length;
+  // for (let i = 0; i < l; i++) {}
+  if (cities == "Paris") {
+    document.getElementById("Paris").style.display = "";
+    document.getElementById("Lyon").style.display = "none";
+    document.getElementById("Montbeliard").style.display = "none";
+  } else if (cities == "Lyon") {
+    document.getElementById("Paris").style.display = "none";
+    document.getElementById("Lyon").style.display = "";
+    document.getElementById("Montbeliard").style.display = "none";
+  } else if (cities == "Montbeliard") {
+    document.getElementById("Paris").style.display = "none";
+    document.getElementById("Lyon").style.display = "none";
+    document.getElementById("Montbeliard").style.display = "";
+  } else {
+    document.getElementById("Paris").style.display = "";
+    document.getElementById("Lyon").style.display = "";
+    document.getElementById("Montbeliard").style.display = "";
+  }
+}
