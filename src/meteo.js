152bf0488b83c1a757c8ab0c7d4d090792d0c675
kamel-ah <kamelahmed147@gmail.com>
2021-02-09 19:51:10 +0100

    Update meteo.js

--- src/meteo.js
+++ src/meteo.js
@@ -1,28 +1,13 @@
 let villeCourante = "Lyon";
 function showCity() {
-  let cities = document.getElementById("pet-select").value;
-  console.log(cities);
+  let nomVilleSelectionnee = document.getElementById("pet-select").value;
+  let cities = document.getElementsByClassName("city");
   let l = cities.length;
-  // for (let i = 0; i < l; i++) {}
-  if (cities == "Paris") {
-    document.getElementById("Paris").style.display = "";
-    document.getElementById("Lyon").style.display = "none";
-    document.getElementById("Montbeliard").style.display = "none";
-  } else if (cities == "Lyon") {
-    document.getElementById("Paris").style.display = "none";
-    document.getElementById("Lyon").style.display = "";
-    document.getElementById("Montbeliard").style.display = "none";
-  } else if (cities == "Montbeliard") {
-    document.getElementById("Paris").style.display = "none";
-    document.getElementById("Lyon").style.display = "none";
-    document.getElementById("Montbeliard").style.display = "";
-  } else if (cities == "") {
-    document.getElementById("Paris").style.display = "none";
-    document.getElementById("Lyon").style.display = "none";
-    document.getElementById("Montbeliard").style.display = "none";
-  } else {
-    document.getElementById("Paris").style.display = "none";
-    document.getElementById("Lyon").style.display = "none";
-    document.getElementById("Montbeliard").style.display = "none";
+  for (i = 0; i < l; i++) {
+    if (cities[i].id == nomVilleSelectionnee) {
+      cities[i].style.display = "";
+    } else {
+      cities[i].style.display = "none";
+    }
   }
 }
